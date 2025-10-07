72262939e7db7ad26e08371e4ca497d8e7798f43
OscarStantion <oscarjeandiaz1@gmail.com>
2025-10-07 08:08:29 -0500

    Mejora la documentación en RolesRepository: agrega comentarios descriptivos para clarificar el propósito del repositorio y sus métodos CRUD.

--- src/main/java/com/example/Integrador/Seguridad/RolesRepository.java
+++ src/main/java/com/example/Integrador/Seguridad/RolesRepository.java
@@ -1,9 +1,9 @@
 package com.example.Integrador.Seguridad;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-
 import com.example.Integrador.Entidades.Roles;
 
+// Repositorio de roles
 public interface RolesRepository extends JpaRepository<Roles, Integer> {
-
+	// Métodos CRUD de roles
 }
