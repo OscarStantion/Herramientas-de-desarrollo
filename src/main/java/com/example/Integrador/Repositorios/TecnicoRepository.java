0615b548f120a260ffd953e7344b74c1f80fcc86
Kingcarakong <waltermollo852@gmail.com>
2025-10-09 18:36:50 -0500

    Actualiza el archivo TecnicoRepository.java para reorganizar las importaciones y mejorar la legibilidad.

--- src/main/java/com/example/Integrador/Repositorios/TecnicoRepository.java
+++ src/main/java/com/example/Integrador/Repositorios/TecnicoRepository.java
@@ -1,10 +1,7 @@
 package com.example.Integrador.Repositorios;
 
-import java.util.Optional;
-
-
-
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
