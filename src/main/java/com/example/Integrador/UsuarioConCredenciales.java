20c48a8410ec0f6b7d0623a1a03f355e7059a15e
OscarStantion <oscarjeandiaz1@gmail.com>
2025-10-09 18:37:01 -0500

    Mejora la documentación en UsuarioConCredenciales: agrega comentarios para clarificar el propósito de la interfaz y sus métodos.

--- src/main/java/com/example/Integrador/UsuarioConCredenciales.java
+++ src/main/java/com/example/Integrador/UsuarioConCredenciales.java
@@ -1,7 +1,9 @@
 package com.example.Integrador;
 
-
+// Usuario con credenciales de acceso
 public interface UsuarioConCredenciales extends TipoUsuario {
+    // Obtiene nombre de usuario
     String getUsuario();
+    // Obtiene contraseña
     String getContraseña();
 }
