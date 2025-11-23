c87f8deb1921e222e0ccbcf79ca910d4fe06b9f5
OscarStantion <oscarjeandiaz1@gmail.com>
2025-11-22 19:02:10 -0500

    feat: Mejorar comentarios y métodos en PlanMantenimientoService

--- src/main/java/com/example/Integrador/Service/PlanMantenimientoService.java
+++ src/main/java/com/example/Integrador/Service/PlanMantenimientoService.java
@@ -8,15 +8,19 @@ import com.example.Integrador.Entidades.PlanMantenimiento;
 import com.example.Integrador.Entidades.RelacionMantenimiento;
 import com.example.Integrador.Repositorios.PlanMantenimientoRepository;
 
+// Servicio plan mantenimiento
 public class PlanMantenimientoService {
 
 	@Autowired
+    // Repositorio planes
     private PlanMantenimientoRepository PlanMantenimiento;
 
+	// Obtener último plan
 	public PlanMantenimientoRepository obtenerUltimaplanMantenimiento() {
-        return (PlanMantenimientoRepository) PlanMantenimiento.findTopByOrderByIdDesc();
-    }
+	        return (PlanMantenimientoRepository) PlanMantenimiento.findTopByOrderByIdDesc();
+	    }
 	
+	 // Obtener todos
 	 public List<PlanMantenimiento> obtenerTodos() {
 	        return PlanMantenimiento.findAll();
 	    }
